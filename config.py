891a23eb68a0ecc6baadaa38550313b69d85801c
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2024-01-14 18:52:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -79,7 +79,7 @@ class Var(object):
     name = str(getenv('name', 'galaxyfilestorebot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002107444913'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = ""
+        URL = "https://filestore-qqs4.onrender.com"
     else:
-        URL = ""
+        URL = "https://filestore-qqs4.onrender.com"
 
 
 
