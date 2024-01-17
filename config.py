b492e5171dd15b0d50901a0f9fd8979ebc115456
GamingBeast1 <116881154+GamingBeast1@users.noreply.github.com>
2024-01-17 10:47:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://galaxyfilestorebot-b1bafe1f5835.herokuapp.com/"
+        URL = ""
     else:
-        URL = "https://galaxyfilestorebot-b1bafe1f5835.herokuapp.com/"
+        URL = ""
 
 
 
